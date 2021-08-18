12f754ec2cc26aa370312e2839de5908db58fbe0
yanalshmilan <yshmilan@gmail.com>
2021-08-18 19:56:25 +0300

    conflict

--- graphql/resolvers/debate/queries.js
+++ graphql/resolvers/debate/queries.js
@@ -1,24 +1,23 @@
-const Tag = require("../../../models/Tag.model");
-const Debate = require("../../../models/Debate.model");
+const Tag = require('../../../models/Tag.model');
+const Debate = require('../../../models/Debate.model');
 const debateQueries = {
   debates: async (_, { order, start, amount, tag, keyword }) => {
     const orderBy = {};
     const filter = {};
-    if (order === "new") orderBy.createdAt = "descending";
-    else if (order === "popularity") orderBy.views = "descending";
-    else if (order === "hot") orderBy.argueCount = "descending";
+    if (order === 'new') orderBy.createdAt = 'descending';
+    else if (order === 'popularity') orderBy.views = 'descending';
+    else if (order === 'hot') orderBy.argueCount = 'descending';
     if (tag) filter.tags = tag;
     if (tag) {
       tag = await Tag.findOne({ title: tag });
       if (tag) filter.tags = tag._id;
       else return [];
     }
-    console.log("here");
     if (keyword) {
-      filter.title = { $regex: ".*" + keyword + ".*", $options: "i" };
+      filter.title = { $regex: '.*' + keyword + '.*', $options: 'i' };
     }
     return await Debate.find(filter)
-      .populate("tags")
+      .populate('tags')
       .sort(orderBy)
       .skip(start)
       .limit(amount);
@@ -27,7 +26,7 @@ const debateQueries = {
     let thisDebate = await Debate.findOneAndUpdate(
       { slug },
       { $inc: { views: 1 } }
-    ).populate("argues");
+    ).populate('argues');
     return thisDebate;
   },
   tags: async () => {
